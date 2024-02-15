9565d32aacb28f1461fcf69451ffd139b9626447
Grace Wang <grace25@bu.edu>
2024-02-15 15:54:15 -0500

    initialied Views and added imports

--- app/src/main/java/com/example/gracewangftoc/MainActivity.kt
+++ app/src/main/java/com/example/gracewangftoc/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.gracewangftoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val seekBarCelsius = findViewById<SeekBar>(R.id.seekBarC)
+        val textViewCelsius = findViewById<TextView>(R.id.textViewC)
+        val seekBarFahrenheit = findViewById<SeekBar>(R.id.seekBarF)
+        val textViewFahrenheit = findViewById<TextView>(R.id.textViewF)
+        val textViewMessage = findViewById<TextView>(R.id.textViewMessage)
     }
 }
